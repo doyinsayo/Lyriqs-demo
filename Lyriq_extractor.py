98a11e0c8782f07084a74f92427d29e0b7742f9d
Doyinsayo Otufadebo <64381567+doyinsayo@users.noreply.github.com>
2022-07-05 21:37:21 +0100

    Create Lyriq_extractor.py

---
+++ Lyriq_extractor.py
@@ -0,0 +1,8 @@
+from lyrics_extractor import SongLyrics 
+
+api_key = 'AIzaSyDM2uJ-CweOkPkZ3xLiyelKDprN8iarBDM'
+engine_ID = 'f93d78b2384fc85e2'
+
+extract_lyriq = SongLyrics(api_key, engine_ID)
+lyriqs = extract_lyriq.get_lyrics("Sungba")
+print(lyriqs)
